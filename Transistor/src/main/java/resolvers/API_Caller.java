77e9014e1b4613d59ad645b7db8a6f5b00c5aeff
huypham37 <129127533+huypham37@users.noreply.github.com>
2024-03-18 14:05:08 +0100

    Move img to resrouces

--- Transistor/src/main/java/resolvers/API_Caller.java
+++ Transistor/src/main/java/resolvers/API_Caller.java
@@ -1,10 +1,10 @@
-package resolvers;
-
-import java.math.BigDecimal;
-
-public class API_Caller {
-    public static BigDecimal call()
-    {
-
-    }
-}
+//package resolvers;
+//
+//import java.math.BigDecimal;
+//
+//public class API_Caller {
+//    public static BigDecimal call()
+//    {
+//
+//    }
+//}
