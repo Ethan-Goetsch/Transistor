68909bf1b018f8462d1b91d2532b3cc2a98a66da
Michael <michalurbnowicz@gmail.com>
2024-03-18 15:13:41 +0100

    test

--- Transistor/src/main/java/resolvers/LocationResolver.java
+++ Transistor/src/main/java/resolvers/LocationResolver.java
@@ -22,108 +22,116 @@ import utils.Coordinates;
 
 public class LocationResolver
 {
+    // "Transistor\\MassZipLatLon.xlsx"
     private String spreadsheetPath;
     private ArrayList<PostCode> postCodes;
 
-    class PostCode
+    private class PostCode
     {
         String name;
         Coordinates cords;
-        
-        public String getName() {
+
+        public String getName()
+        {
             return name;
         }
-        public void setName(String name) {
+
+        public void setName(String name)
+        {
             this.name = name;
         }
-        public Coordinates getCords() {
+
+        public Coordinates getCords()
+        {
             return cords;
         }
-        public void setCords(Coordinates cords) {
+
+        public void setCords(Coordinates cords)
+        {
             this.cords = cords;
         }
+
         public PostCode(String name, Coordinates cords)
         {
             this.name = name;
             this.cords = cords;
         }
-    
+
     }
 
     public LocationResolver(String spreadsheetPath)
     {
         this.spreadsheetPath = spreadsheetPath;
         this.postCodes = new ArrayList<PostCode>();
+
+        loadData();
+
+        System.out.println("Loaded " + postCodes.size() + " post codes");
     }
 
-    public void readData()
+    // https://stackoverflow.com/questions/37811334/how-to-read-excel-xlsx-file-in-java
+    private void loadData()
     {
-        try 
+        try
         {
             FileInputStream excelFile = new FileInputStream(new File(spreadsheetPath));
             XSSFWorkbook wb = new XSSFWorkbook(excelFile);
             XSSFSheet sheet = wb.getSheetAt(0);
 
             int rowCount;
-            rowCount= sheet.getPhysicalNumberOfRows();
+            rowCount = sheet.getPhysicalNumberOfRows();
             System.out.println("row count: " + rowCount);
 
-            // iterate the excel file
+            // i = 1 since we skip the first row
             int i = 1;
             Iterator<Row> rowIterator = sheet.iterator();
 
-            //skip the first row
+            // skip the first row
             rowIterator.next();
-        do
-        {
-
-            Row row = rowIterator.next();
-            Iterator<Cell> cellIterator = row.cellIterator();
-
-            String postName = "";
-            String postLatitudeStr = "";
-            String postLongtitudeStr = "";
-
-            while(cellIterator.hasNext())
+            do
             {
-                Cell cell = cellIterator.next();
-                    if(cell.getColumnIndex()==0)
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+
+                String postName = "";
+                String postLatitudeStr = "";
+                String postLongtitudeStr = "";
+
+                while (cellIterator.hasNext())
+                {
+                    Cell cell = cellIterator.next();
+                    if (cell.getColumnIndex() == 0)
                     {
                         DataFormatter df = new DataFormatter();
                         postName = df.formatCellValue(cell);
                     }
-                    if(cell.getColumnIndex()==1)
+                    if (cell.getColumnIndex() == 1)
                     {
                         DataFormatter df = new DataFormatter();
                         postLatitudeStr = df.formatCellValue(cell);
                     }
-                    if(cell.getColumnIndex()==2)
+                    if (cell.getColumnIndex() == 2)
                     {
                         DataFormatter df = new DataFormatter();
                         postLongtitudeStr = df.formatCellValue(cell);
                     }
+                }
+
+                Coordinates newCords = new Coordinates(Double.parseDouble(postLatitudeStr), Double.parseDouble(postLongtitudeStr));
+                PostCode newPostCode = new PostCode(postName, newCords);
+
+                postCodes.add(newPostCode);
+
+                i++;
+
+                if (i > rowCount)
+                {
+                    break;
+                }
             }
+            while (rowIterator.hasNext());
 
-
-
-            //System.out.println(postName + " " + postLatitudeStr + " " + postLongtitudeStr);
-
-            Coordinates newCords = new Coordinates(Double.parseDouble(postLatitudeStr), Double.parseDouble(postLongtitudeStr));
-            PostCode newPostCode = new PostCode(postName, newCords);
-
-            postCodes.add(newPostCode);
-
-            i++;
-
-            if(i > rowCount)
-            {
-                break;
-            }
-            //break;
-        }
-        while(rowIterator.hasNext());
-
-        wb.close();
+            wb.close();
 
         }
         catch (Exception e)
@@ -131,6 +139,18 @@ public class LocationResolver
             System.out.println("fe: " + e.getMessage());
             return;
         }
+    }
+
+    public Coordinates getCordsFromFile(String postName)
+    {
+        for (PostCode postCode : postCodes)
+        {
+            if (postCode.getName().equalsIgnoreCase(postName))
+            {
+                return postCode.getCords();
+            }
+        }
+        return null;
     }
 
     public void printCodes()
@@ -143,10 +163,16 @@ public class LocationResolver
 
     public static void debug()
     {
+        //6211AL	50.85523285	5.692237193
+        //6212EA	50.83560203	5.689429256
+        //6229ZE	50.82092283	5.708232104
+
         System.out.println("ltest");
         LocationResolver locationResolver = new LocationResolver("Transistor\\MassZipLatLon.xlsx");
-        locationResolver.readData();
-        locationResolver.printCodes();
+        System.out.println(locationResolver.getCordsFromFile("6211AL").getLatitude() + " " + locationResolver.getCordsFromFile("6211AL").getLongtitude());
+        System.out.println(locationResolver.getCordsFromFile("6212EA").getLatitude() + " " + locationResolver.getCordsFromFile("6212EA").getLongtitude());
+        System.out.println(locationResolver.getCordsFromFile("6229ze").getLatitude() + " " + locationResolver.getCordsFromFile("6229ze").getLongtitude());
+        
     }
 
 }
