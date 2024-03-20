6034a62f56be77885d0410f1c0ce14087f527da3
Michael <michalurbnowicz@gmail.com>
2024-03-20 12:48:32 +0100

    merge

--- Transistor/src/main/java/calculators/RouteCalculator.java
+++ Transistor/src/main/java/calculators/RouteCalculator.java
@@ -1,6 +1,7 @@
 package calculators;
 
 import entities.TransportType;
+import utils.Coordinates;
 import utils.TransportationType;
 import static utils.TransportationTypeCreated.*;
 
@@ -8,28 +9,18 @@ public class RouteCalculator
 {
     private static final int radiusEarthInM = 6371000;
 
-    public double Distance2P(double lat1, double lon1, double lat2, double lon2)
+    public double Distance2P(Coordinates point1, Coordinates point2)
     {
-        lat1 = Math.toRadians(lat1);
-        lon1 = Math.toRadians(lon1);
-        lat2 = Math.toRadians(lat2);
-        lon2 = Math.toRadians(lon2);
+        double lat1 = Math.toRadians(point1.getLatitude());
+        double lon1 = Math.toRadians(point1.getLongitude());
+        double lat2 = Math.toRadians(point2.getLatitude());
+        double lon2 = Math.toRadians(point2.getLongitude());
         return Math.acos(Math.sin(lat1)*Math.sin(lat2)+Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1))*radiusEarthInM;
     }
-//    public double KilometerTimeTaken(double distance)
-//    {
-//        double kiloPerHour=(distance/1000)/5;
-//        return kiloPerHour;
-//    }
-//    public double MeterTimeTaken(double distance)
-//    {
-//        double MeterPerSecond=distance/1.4;
-//        return MeterPerSecond;
-//    }
 
-    public double calculateTime(TransportType type, double lat1, double lon1, double lat2, double lon2)
+    public double calculateTime(TransportType type, Coordinates point1, Coordinates point2)
     {
-        double distance = Distance2P( lat1, lon1, lat2, lon2);
+        double distance = Distance2P(point1, point2);
         return calculateTime(type, distance);
     }
 
