8354f2fce283d3fb65c85d168f8702cfeb3e7302
Nikitathepita <163841327+Nikitathepita@users.noreply.github.com>
2024-03-20 12:56:03 +0100

    Bye

--- Transistor/src/main/java/calculators/RouteCalculator.java
+++ Transistor/src/main/java/calculators/RouteCalculator.java
@@ -1,10 +1,9 @@
 package calculators;
 
 import entities.TransportType;
-import utils.TransportationType;
-import static utils.TransportationTypeCreated.*;
+import utils.Conversions;
 
-public class RouteCalculator
+public class RouteCalculator extends Conversions
 {
     private static final int radiusEarthInM = 6371000;
 
@@ -16,26 +15,20 @@ public class RouteCalculator
         lon2 = Math.toRadians(lon2);
         return Math.acos(Math.sin(lat1)*Math.sin(lat2)+Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1))*radiusEarthInM;
     }
-//    public double KilometerTimeTaken(double distance)
-//    {
-//        double kiloPerHour=(distance/1000)/5;
-//        return kiloPerHour;
-//    }
-//    public double MeterTimeTaken(double distance)
-//    {
-//        double MeterPerSecond=distance/1.4;
-//        return MeterPerSecond;
-//    }
-
     public double calculateTime(TransportType type, double lat1, double lon1, double lat2, double lon2)
     {
         double distance = Distance2P( lat1, lon1, lat2, lon2);
         return calculateTime(type, distance);
     }
-
     public double calculateTime(TransportType type, double distance)
     {
         double speed = type.getSpeedInMetersPerSecond();
         return distance/speed;
+    }
+    public static void main(String[] args) {
+        RouteCalculator calculator= new RouteCalculator();
+       double distance=calculator.Distance2P(50.835039885712746, 5.754273381674836,50.831390750247856, 5.7577784667444725);
+        System.out.println(toKm(distance));
+        System.out.println(timeDivision(calculator.calculateTime(TransportType.WALKING,distance)));
     }
 }
