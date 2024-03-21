b422aa6ef88af4f87ab99c5f0bbd2603fb489eee
EthanGoetsch <ethangoetschhome@gmail.com>
2024-03-21 09:06:47 +0100

    Added MassLocationFile to PathFiles

--- Transistor/src/main/java/Program.java
+++ Transistor/src/main/java/Program.java
@@ -2,12 +2,13 @@ import application.ApplicationManager;
 import calculators.RouteCalculator;
 import resolvers.LocationResolver;
 import ui.UIController;
+import utils.PathFiles;
 
 public class Program
 {
     public static void main(String[] args)
     {
-        LocationResolver locationResolver = new LocationResolver("Transistor/src/main/resources/MassZipLatLon.xlsx");
+        LocationResolver locationResolver = new LocationResolver(PathFiles.MassLocationFile);
         RouteCalculator routeCalculator = new RouteCalculator();
         ApplicationManager manager = new ApplicationManager(locationResolver, routeCalculator);
         UIController controller = new UIController(manager);
--- Transistor/src/main/java/utils/PathFiles.java
+++ Transistor/src/main/java/utils/PathFiles.java
@@ -4,4 +4,5 @@ public class PathFiles
 {
     public static final String GraphFile = "Transistor/src/main/resources/Maastricht.osm.pbf";
     public static final String GraphResourceFolder = "Transistor/src/main/resources/graph";
+    public static final String MassLocationFile = "Transistor/src/main/resources/MassZipLatLon.xlsx";
 }
