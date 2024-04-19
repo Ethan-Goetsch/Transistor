f8cdc031eec4dd8c75969580fe39107fdc13f19d
Dani Angelov <138165137+aFriendOfDorothy@users.noreply.github.com>
2024-04-19 13:08:24 +0200

    Added different types of plotting

--- Transistor/src/main/java/ui/CustomComponents/MapViewer.java
+++ Transistor/src/main/java/ui/CustomComponents/MapViewer.java
@@ -2,6 +2,7 @@ package ui.CustomComponents;
 
 import com.graphhopper.ResponsePath;
 import com.graphhopper.util.shapes.GHPoint3D;
+import entities.RouteType;
 import org.jxmapviewer.JXMapViewer;
 import org.jxmapviewer.viewer.GeoPosition;
 import java.awt.*;
@@ -11,7 +12,11 @@ import java.util.function.Consumer;
 public class MapViewer extends JXMapViewer {
     private ResponsePath bestPath;
     private boolean firstPoint;
-    public MapViewer(){}
+
+    private RouteType routeType;
+    public MapViewer(){
+        routeType = RouteType.ACTUAL;
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -22,19 +27,34 @@ public class MapViewer extends JXMapViewer {
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             Path2D p = new Path2D.Double();
             firstPoint = true;
-            bestPath.getPoints().forEach(new Consumer<GHPoint3D>() {
-                @Override
-                public void accept(GHPoint3D ghPoint3D) {
-                    Point2D point = convertGeoPositionToPoint(new GeoPosition(ghPoint3D.getLat(),ghPoint3D.getLon() ));
-                    if(firstPoint){
-                        firstPoint = false;
-                        p.moveTo(point.getX(), point.getY());
-                    }else{
-                        p.lineTo(point.getX(), point.getY());
+            if(routeType == RouteType.ACTUAL){
+                bestPath.getPoints().forEach(new Consumer<GHPoint3D>() {
+                    @Override
+                    public void accept(GHPoint3D ghPoint3D) {
+                        Point2D point = convertGeoPositionToPoint(new GeoPosition(ghPoint3D.getLat(),ghPoint3D.getLon() ));
+                        if(firstPoint){
+                            firstPoint = false;
+                            p.moveTo(point.getX(), point.getY());
+                        }else{
+                            p.lineTo(point.getX(), point.getY());
+                        }
                     }
-                }
-            });
-
+                });
+            }
+            else{
+                bestPath.getWaypoints().forEach(new Consumer<GHPoint3D>() {
+                    @Override
+                    public void accept(GHPoint3D ghPoint3D) {
+                        Point2D point = convertGeoPositionToPoint(new GeoPosition(ghPoint3D.getLat(),ghPoint3D.getLon() ));
+                        if(firstPoint){
+                            firstPoint = false;
+                            p.moveTo(point.getX(), point.getY());
+                        }else{
+                            p.lineTo(point.getX(), point.getY());
+                        }
+                    }
+                });
+            }
             g2.setColor(new Color(12, 18, 222));
             g2.setStroke(new BasicStroke(5f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
             g2.draw(p);
@@ -43,5 +63,8 @@ public class MapViewer extends JXMapViewer {
     }
     public void bestPath(ResponsePath bestPath){
         this.bestPath = bestPath;
+    }
+    public void changeRouteType(RouteType routeType){
+        this.routeType = routeType;
     }
 }
