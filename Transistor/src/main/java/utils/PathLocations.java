27fbed2c093734d9dca40de3fe43fd29e653f54e
EthanGoetsch <ethangoetschhome@gmail.com>
2024-04-30 10:03:12 +0200

    Added Credentials path to PathLocations

--- Transistor/src/main/java/utils/PathLocations.java
+++ Transistor/src/main/java/utils/PathLocations.java
@@ -7,4 +7,5 @@ public class PathLocations
     public static final String GRAPH_FILE = "Transistor/src/main/resources/Maastricht.osm.pbf";
     public static final String GRAPH_RESOURCE_FOLDER = "Transistor/src/main/resources/graph";
     public static final String MASS_LOCATION_FILE = "Transistor/src/main/resources/MassZipLatLon.xlsx";
+    public static final String CREDENTIALS_FILE = "Transistor/src/main/resources/Credentials.txt";
 }
