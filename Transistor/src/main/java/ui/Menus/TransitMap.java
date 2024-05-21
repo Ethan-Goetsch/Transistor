bda55f0e0883419fb99d210b4b532304f1da6ef5
Dani Angelov <138165137+aFriendOfDorothy@users.noreply.github.com>
2024-05-21 16:20:09 +0200

    Transitmap

--- Transistor/src/main/java/ui/Menus/TransitMap.java
+++ Transistor/src/main/java/ui/Menus/TransitMap.java
@@ -9,14 +9,23 @@ import javax.swing.*;
 
 public class TransitMap extends JMenu {
     private final JFrame mainWindow;
+    private boolean displayed;
 
     public TransitMap(JFrame mainWindow) {
         super("Transit Map");
         this.mainWindow = mainWindow;
+        displayed = false;
+        this.addActionListener(e -> {displayAllRoutes();});
     }
 
     private void displayAllRoutes(){
         JXMapViewer map = ((MainWindow)mainWindow).getjXMapViewer();
+        if(!displayed){
+
 //        ((MapViewer) map).setPaths(paths);//List<Path>
+        }else{
+            ((MapViewer) map).setPaths(null);//List<Path>
+        }
+
     }
 }
