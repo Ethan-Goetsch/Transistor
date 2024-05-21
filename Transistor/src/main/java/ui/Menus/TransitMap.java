61bdb6c69178f4cb9dc20313e1a29f80423f75dd
Michael <srej100@gmail.com>
2024-05-21 16:49:24 +0200

    transit map

--- Transistor/src/main/java/ui/Menus/TransitMap.java
+++ Transistor/src/main/java/ui/Menus/TransitMap.java
@@ -1,31 +1,128 @@
 package ui.Menus;
 
 import org.jxmapviewer.JXMapViewer;
+
+import database.DatabaseManager;
+import database.queries.TransitMapDataQuery;
 import ui.CustomComponents.MapViewer;
 import ui.MMap;
 import ui.MainWindow;
+import entities.Coordinate;
+import entities.Path;
+import entities.PathPoint;
+import entities.PointType;
+import entities.gtfs.GDisplayRoute;
+import entities.gtfs.GShapePoint;
+import java.awt.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.*;
 
-public class TransitMap extends JMenu {
+public class TransitMap extends JMenu
+{
     private final JFrame mainWindow;
     private boolean displayed;
 
-    public TransitMap(JFrame mainWindow) {
+    private List<Color> pathColors;
+    private List<Path> displayPaths;
+
+    public TransitMap(JFrame mainWindow)
+    {
         super("Transit Map");
         this.mainWindow = mainWindow;
         displayed = false;
-        this.addActionListener(e -> {displayAllRoutes();});
+        this.addActionListener(e -> {
+            displayAllRoutes();
+        });
+
+        pathColors = new ArrayList<>();
+        displayPaths = new ArrayList<>();
+        populatePathColors();
+        populateDisplayPaths();
     }
 
-    private void displayAllRoutes(){
-        JXMapViewer map = ((MainWindow)mainWindow).getjXMapViewer();
-        if(!displayed){
+    private void populateDisplayPaths()
+    {
+        List<GDisplayRoute> displayRoutes = DatabaseManager.executeAndReadQuery(new TransitMapDataQuery());
 
-//        ((MapViewer) map).setPaths(paths);//List<Path>
-        }else{
+        int iter = 0;
+        for (GDisplayRoute displayRoute : displayRoutes)
+        {
+            List<GShapePoint> displayRoutePoints = displayRoute.getShapePoints();
+            if (displayRoutePoints.isEmpty())
+            {
+                displayRoutePoints = displayRoute.getShapePoints();
+            }
+
+            List<PathPoint> pathPoints = new ArrayList<>();
+
+            for (GShapePoint displayRoutePoint : displayRoutePoints)
+            {
+                pathPoints.add(new PathPoint(new Coordinate(displayRoutePoint.getLatitude(), displayRoutePoint.getLongitude()), PointType.Normal));    
+            }
+
+            displayPaths.add(new Path(pathPoints, pathColors.get(iter % pathColors.size())));
+            iter++;
+        }
+
+    }
+
+    private void displayAllRoutes()
+    {
+        System.out.println("clicked");
+        JXMapViewer map = ((MainWindow) mainWindow).getjXMapViewer();
+        if (!displayed)
+        {
+
+            ((MapViewer) map).setPaths(displayPaths);//List<Path>
+        }
+        else
+        {
             ((MapViewer) map).setPaths(null);//List<Path>
         }
 
+    }
+
+    public void populatePathColors()
+    {
+        pathColors.add(convertToColor(0xFFFFB300)); //Vivid Yellow
+        pathColors.add(convertToColor(0xFF803E75)); //Strong Purple
+        pathColors.add(convertToColor(0xFFFF6800)); //Vivid Orange
+        pathColors.add(convertToColor(0xFFA6BDD7)); //Very Light Blue
+        pathColors.add(convertToColor(0xFFC10020)); //Vivid Red
+        pathColors.add(convertToColor(0xFFCEA262)); //Grayish Yellow
+        pathColors.add(convertToColor(0xFF817066)); //Medium Gray
+    
+        //The following will not be good for people with defective color vision
+        pathColors.add(convertToColor(0xFF007D34)); //Vivid Green
+        pathColors.add(convertToColor(0xFFF6768E)); //Strong Purplish Pink
+        pathColors.add(convertToColor(0xFF00538A)); //Strong Blue
+        pathColors.add(convertToColor(0xFFFF7A5C)); //Strong Yellowish Pink
+        pathColors.add(convertToColor(0xFF53377A)); //Strong Violet
+        pathColors.add(convertToColor(0xFFFF8E00)); //Vivid Orange Yellow
+        pathColors.add(convertToColor(0xFFB32851)); //Strong Purplish Red
+        pathColors.add(convertToColor(0xFFF4C800)); //Vivid Greenish Yellow
+        pathColors.add(convertToColor(0xFF7F180D)); //Strong Reddish Brown
+        pathColors.add(convertToColor(0xFF93AA00)); //Vivid Yellowish Green
+        pathColors.add(convertToColor(0xFF593315)); //Deep Yellowish Brown
+        pathColors.add(convertToColor(0xFFF13A13)); //Vivid Reddish Orange
+        pathColors.add(convertToColor(0xFF232C16)); //Dark Olive Green
+
+    }
+
+    public static Color convertToColor(long unsignedInt)
+    {
+        // Mask the lower 24 bits to get the RGB values
+        int rgb = (int) (unsignedInt & 0xFFFFFF);
+
+        // Extract individual color components
+        int red = (rgb >> 16) & 0xFF;
+        int green = (rgb >> 8) & 0xFF;
+        int blue = rgb & 0xFF;
+
+        // Create and return the Color object
+        return new Color(red, green, blue);
     }
 }
