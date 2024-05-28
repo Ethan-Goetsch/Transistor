732000cc37a68ddf362915a06f31ad83315de6fb
Khosvoi <v.kosatik@student.maastrichtuniversity.nl>
2024-05-28 13:17:13 +0200

    Added and updated Tests.java

--- Transistor/src/test/java/Tests.java
+++ Transistor/src/test/java/Tests.java
@@ -1,31 +1,9 @@
 import calculators.*;
 import database.DatabaseManager;
-import database.queries.GetAllStopsForTrip;
-import database.queries.GetRouteForTripQuery;
-import database.queries.GetTripBetweenTwoStopsQuery;
 import entities.*;
-import entities.transit.TransitNode;
-import entities.transit.TransitRoute;
-import entities.transit.TransitTrip;
-import org.apache.poi.hpsf.Decimal;
-import org.checkerframework.checker.units.qual.C;
-import org.jetbrains.annotations.TestOnly;
-import static entities.RouteType.*;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.Test;
-import resolvers.LocationResolver;
-import utils.PathLocations;
-import java.math.RoundingMode;
-import java.sql.Connection;
-import java.sql.SQLOutput;
-import java.text.DecimalFormat;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-
-//? 1. Different coordinates. 2. Null coordinates. 3. Non-existent coordinates. 4 same coordinates
 
 class Tests {
 
@@ -34,8 +12,6 @@ class Tests {
         DatabaseManager.getConnection("src/test/resources/credentials.txt");
     }
 
-
-    private final DecimalFormat df = new DecimalFormat("0.00");
     @Test
     void aerialCalcTest() {
         // test for aerial calculation from different coords 50.85523285, 5.692237193 to 50.84027704, 5.68972678
@@ -46,7 +22,7 @@ class Tests {
         LocalTime arrivalTime = LocalTime.parse("00:00:00");
         var route = new RouteCalculationRequest(departure, arrival,departureTime,arrivalTime, TransportType.FOOT);
         var resultTrip = calc.calculateRoute(route);
-        assertNotNull(resultTrip.getArrivalTime());
+        assertNotNull(resultTrip.path());
     }
 
     @Test
@@ -59,21 +35,23 @@ class Tests {
         LocalTime arrivalTime = LocalTime.parse("00:00:00");
         var route = new RouteCalculationRequest(departure, arrival,departureTime,arrivalTime, TransportType.FOOT);
         var resultTrip = calc.calculateRoute(route);
-        assertNotNull(resultTrip.getArrivalTime());
+        assertNotNull(resultTrip.path());
     }
 
     @Test
     void busPathTest1() {
+        //test for actual path between two stops
         var transitCalculator = new TransitCalculator();
         var trip = transitCalculator.calculateRoute(2578413, 2578366);
         assertNotNull(trip);
         LocalTime expectedArrivalTime = LocalTime.parse("08:32:00");
         assertEquals(expectedArrivalTime, trip.getArrivalTime());
+        assertNotNull(trip.path());
     }
-
 
     @Test
     void busPathTest2(){
+        //test for path between same stops
         var transitCalculator = new TransitCalculator();
         var trip = transitCalculator.calculateRoute(2578413, 2578413);
         assertNull(trip);
@@ -82,9 +60,17 @@ class Tests {
 
     @Test
     void busPathTest3(){
+        //test for path between non-existing stops
         var transitCalculator = new TransitCalculator();
         var trip = transitCalculator.calculateRoute(999999, 999999);
         assertNull(trip);
     }
 
+    @Test
+    void busPathTest4(){
+        //test for path between existing and not existing stop
+        var transitCalculator = new TransitCalculator();
+        var trip = transitCalculator.calculateRoute(2578413, 999999);
+        assertNull(trip);
+    }
 }
