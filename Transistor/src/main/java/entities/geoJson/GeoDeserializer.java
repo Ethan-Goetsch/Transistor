45a80812615629959fa8be450d69c28a0e3ae8e8
Khosvoi <v.kosatik@student.maastrichtuniversity.nl>
2024-06-19 19:04:17 +0200

    fixed the geojson reading

--- Transistor/src/main/java/entities/geoJson/GeoDeserializer.java
+++ Transistor/src/main/java/entities/geoJson/GeoDeserializer.java
@@ -11,7 +11,28 @@ public class GeoDeserializer {
     public static Map<String, Object> readGeoFile(String filePath) throws IOException {
         return objectMapper.readValue(new File(filePath), Map.class);
     }
-
+    static String[] allowedShopTypes = {
+            "supermarket",
+            "greengrocer",
+            "bakery",
+            "butcher",
+            "medical_supply",
+            "convenience",
+            "hairdresser",
+            "nutrition_supplements",
+            "optician",
+            "books",
+            "car_parts",
+            "car",
+            "sports",
+            "massage",
+            "storage_rental",
+            "hearing_aids",
+            "outdoor",
+            "soft_drugs",
+            "dry_cleaning",
+            "market"
+    };
     public static List<GeoData> deserializeGeoData(String filePath, String type) throws IOException {
         List<GeoData> result = new ArrayList<>();
 
@@ -21,10 +42,10 @@ public class GeoDeserializer {
         for (Map<String, Object> feature : features) {
             Map<String, Object> geometry = (Map<String, Object>) feature.get("geometry");
             List<Double> coordinates = (List<Double>) geometry.get("coordinates");
-            String exactType = type;
-            if("amenity".equals(type)){
-                exactType = (String) ((Map<String, Object>) feature.get("properties")).get("amenity");
-            }
+            String exactType = (String) ((Map<String, Object>) feature.get("properties")).get(type);
+            if(!Arrays.asList(allowedShopTypes).contains(exactType) == false)
+                continue;
+
             // in geojson files the coordinates start with longitude and then latitude
             GeoData geoData = new GeoData(exactType, coordinates.get(1), coordinates.get(0));
             result.add(geoData);
